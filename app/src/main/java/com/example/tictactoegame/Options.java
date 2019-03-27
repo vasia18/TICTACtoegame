2103fe8b48502b58880284d98a270f8aa4e52d7c
Arkadiy Ivanov <studiovasia18@gmail.com>
2019-03-27 18:27:50 +0300

    Переписал приложение(27.03.19)2

---
+++ app/src/main/java/com/example/tictactoegame/Options.java
@@ -0,0 +1,58 @@
+package com.example.tictactoegame;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class Options extends AppCompatActivity {
+
+
+    // private RadioButton easy_radiobutton;
+    // private RadioButton medium_radiobutton;
+    // private RadioButton hard_radiobutton;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        // Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options);
+        initButton();
+    }
+
+    private void initButton() {
+
+        RadioButton easy = (RadioButton) findViewById(R.id.easy_radiobutton);
+        easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TicTacToeModel.getInstance().setDifficulty(TicTacToeModel.EASY_DIF);
+            }
+        });
+
+        RadioButton medium = (RadioButton) findViewById(R.id.medium_radiobutton);
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TicTacToeModel.getInstance().setDifficulty(TicTacToeModel.MEDIUM_DIF);
+            }
+        });
+
+        RadioButton hard = (RadioButton) findViewById(R.id.hard_radiobutton);
+        hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TicTacToeModel.getInstance().setDifficulty(TicTacToeModel.HARD_DIF);
+            }
+        });
+
+        //switch (TicTacToy.getTnstance().getDifficulty())
+        //  case TicTacToy
+
+
+    }
+
+}
